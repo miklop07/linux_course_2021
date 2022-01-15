c257e84cdb743caebd8dec919f18e1bbca8a717b
miklop <miklop07@gmail.com>
2022-01-15 11:55:20 +0300

    changes

--- 06_TracingFencing/move.c
+++ 06_TracingFencing/move.c
@@ -3,7 +3,7 @@
 
 int drop_errno() {
     if (errno) {
-        return 1;
+        return errno;
     }
     return 0;
 }
@@ -24,75 +24,82 @@ int main(int argc, char** argv) {
         return errno;
     }
 
+    int prev_errno;
+
     FILE* out;
     if (!(out = fopen(argv[2], "w"))) {
         perror("fopen out error");
+        prev_errno = errno;
         fclose(in);
         if (drop_errno()) {
             perror("fclose in error");
-            return errno;
+            return prev_errno;
         }
-        return errno;
+        return prev_errno;
     }
 
     char c;
-    while((c = fgetc(in)) != EOF){
+    while((c = fgetc(in)) != EOF) {
         if(fputc(c, out) == EOF){
             perror("fputc error");
             fclose(in);
+            prev_errno = errno;
             if (drop_errno()) {
                 perror("fclose in error");
-                return errno;
+                return prev_errno;
             }
             fclose(out);
             if (drop_errno()) {
                 perror("fclose out error");
-                return errno;
+                return prev_errno;
             }
             remove(argv[2]);
             if (drop_errno()) {
                 perror("remove out error");
-                return errno;
+                return prev_errno;
             }
-            return errno;
+            return prev_errno;
         }
     }
     if (errno) {
+        prev_errno = errno;
         perror("fgetc error");
         fclose(in);
         if (drop_errno()) {
             perror("fclose in error");
-            return errno;
+            return prev_errno;
         }
         fclose(out);
         if (drop_errno()) {
             perror("fclose out error");
-            return errno;
+            return prev_errno;
         }
         remove(argv[2]);
         if (drop_errno()) {
             perror("remove out error");
-            return errno;
+            return prev_errno;
         }
-        return errno;
+        return prev_errno;
     }
 
     fclose(in);
+    prev_errno = errno;
     if (drop_errno()) {
         perror("fclose in error");
-        return errno;
+        return prev_errno;
     }
     fclose(out);
+    prev_errno = errno;
     if (drop_errno()) {
         perror("fclose out error");
-        return errno;
+        return prev_errno;
     }
     remove(argv[1]);
+    prev_errno = errno;
     if (drop_errno()) {
         perror("remove in error");
-        return errno;
+        return prev_errno;
     }
-    return errno;
 
     return 0;
 }
---
+++ 06_TracingFencing/tests.sh
@@ -0,0 +1,3 @@
+LD_PRELOAD=./pre_lib.so strace -P _PROTECTED_ -e trace=read -e inject=read:error=EISDIR:when=2 ./move _PROTECTED_ _PROTECTED_mv
+if [ $? -eq 21 ] && [ -f _PROTECTED_ ] && [ ! -f _PROTECTED_mv ]; then echo "OK"; else echo "FAILED"; fi
+echo $?
